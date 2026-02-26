111ae9c5c73c5a8251c65ff0e74295c5ec668c18
ZainelevenAir <2504633@students.au.edu.pk>
2026-02-26 10:33:10 +0500

    main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+import { add, subtract } from './math-utils.js';
+
+const sum = add(10, 5);
+const difference = subtract(20, 7);
+
+console.log(`The sum of 10 + 5 is: ${sum}`);
+console.log(`The difference of 20 - 7 is: ${difference}`);
